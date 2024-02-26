d5772b185a3215f8cc95821b00391bca395a62b9
Kat Tschan <kat@kattschan.co.uk>
2024-02-26 13:02:48 -0500

    the new sote

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Files for my website
+
 I will likely forget this README exists and never touch it again.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,67 @@
 <div class="size-full">
-<div class="sm:size-full md:size-11/12 lg:size-11/12 xl:size-11/12 bg-violet-500">
-<h1 class="text-slate-200 text-4xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
+	<div class="flex justify-center items-center h-screen">
+		<div class="bg-violet-500 size-11/12 rounded-md px-4">
+			<div class="flex">
+				<h1 class="text-slate-200 text-4xl">Hellow! I'm Kat Tschan</h1>
+				<pre>(she/her)</pre>
+			</div>
+			<!--Seperator-->
+			<div class="h-1 bg-violet-900"></div>
+			<h2 class="text-slate-300 text-2xl">About me</h2>
+			<p>
+				I'm a transfem software developer. I like working with Javascript and HTML along with
+				SvelteKit. I'm also learning some mobile technologies, namely Swift for iOS and Kotlin for
+				Android.
+			</p>
+			<!--Seperator-->
+			<div class="h-1 bg-violet-900"></div>
+			<h2 class="text-slate-300 text-2xl">Projects</h2>
+			<ul>
+				<li>
+					<a href="https://mastoemoji.katsoftware.tech">Mastodon Emoji Viewer</a> - A tool to view all
+					the emoji on a Mastodon instance.
+				</li>
+				<li>
+					<a href="https://github.com/kattschan/kattschan.co.uk">This website</a> - My personal website
+					showcasing my projects and socials.
+				</li>
+			</ul>
+			<!--Seperator-->
+			<div class="h-1 bg-violet-900"></div>
+			<h2 class="text-slate-300 text-2xl">Socials</h2>
+			<ul>
+				<li>
+					<a href="https://wetdry.world/@kat" rel="me">Mastodon</a>
+				</li>
+				<li>
+					<a href="https://github.com/kattschan">GitHub</a>
+				</li>
+				<li>Discord: kat.js</li>
+				<li>Matrix: @kat:cinderblock.moe</li>
+			</ul>
+
+			<!--Seperator-->
+			<div class="h-1 bg-violet-900"></div>
+			<h2 class="text-slate-300 text-2xl">Random Slider for giggles</h2>
+			<input
+				type="range"
+				min="0"
+				max="1000000"
+				value="50"
+				class="slider"
+				id="myRange"
+				width="100%"
+				style="
+                    -webkit-appearance: none;
+                    width: 100%;
+                    height: 25px;
+                    background: #d3d3d3;
+                    outline: none;
+                    opacity: 0.7;
+                    -webkit-transition: .2s;
+                    transition: opacity .2s;
+                    border-radius: 5px;"
+			/>
+		</div>
+	</div>
 </div>
